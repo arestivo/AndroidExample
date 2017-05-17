bb3a1c510f94112aad158041aeaeea7fafabbfca
Andre Restivo <andre.restivo@gmail.com>
2017-05-17 16:59:19 +0100

    Layout

--- app/src/main/java/com/aor/androidexample/ListActivity.java
+++ app/src/main/java/com/aor/androidexample/ListActivity.java
@@ -2,36 +2,41 @@ package com.aor.androidexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ListActivity extends AppCompatActivity {
     private static final String STATE_USERNAME = "USERNAME";
     private static final String STATE_PASSWORD = "PASSWORD";
 
-    private EditText mUsername;
-    private EditText mPassword;
+    private EditText editUsername;
+    private EditText editPassword;
+
+    private Button buttonAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        mUsername = (EditText) findViewById(R.id.edit_username);
-        mPassword = (EditText) findViewById(R.id.edit_username);
+        editUsername = (EditText) findViewById(R.id.edit_name);
+        editPassword = (EditText) findViewById(R.id.edit_email);
+
+        buttonAdd = (Button) findViewById(R.id.button_add_person);
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putString(STATE_USERNAME, mUsername.getText().toString());
-        savedInstanceState.putString(STATE_PASSWORD, mPassword.getText().toString());
+        savedInstanceState.putString(STATE_USERNAME, editUsername.getText().toString());
+        savedInstanceState.putString(STATE_PASSWORD, editPassword.getText().toString());
         super.onSaveInstanceState(savedInstanceState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        mUsername.setText(savedInstanceState.getString(STATE_USERNAME));
-        mPassword.setText(savedInstanceState.getString(STATE_PASSWORD));
+        editUsername.setText(savedInstanceState.getString(STATE_USERNAME));
+        editPassword.setText(savedInstanceState.getString(STATE_PASSWORD));
     }
 }
