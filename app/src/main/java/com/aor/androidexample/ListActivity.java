48caf9ebaefb5dad1d11aca1cbe015d0265b36da
Andre Restivo <andre.restivo@gmail.com>
2017-05-17 13:00:04 +0100

    Activity lifecyle logging

--- app/src/main/java/com/aor/androidexample/ListActivity.java
+++ app/src/main/java/com/aor/androidexample/ListActivity.java
@@ -2,6 +2,7 @@ package com.aor.androidexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -9,5 +10,41 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Log.d("AndroidExample", "ON CREATE");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onResume();
+
+        Log.d("AndroidExample", "ON START");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("AndroidExample", "ON STOP");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("AndroidExample", "ON PAUSE");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("AndroidExample", "ON RESUME");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d("AndroidExample", "ON RESTART");
     }
 }
